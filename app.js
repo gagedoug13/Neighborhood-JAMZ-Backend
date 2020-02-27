0424715c3d7b71be4fbdddd741aa4c4d0e0ad2ed
Gage Thornberry <gagedoug13@gmail.com>
2020-02-27 14:35:37 -0700

    The backend is now handling successful responses as well as errors.

--- app.js
+++ app.js
@@ -6,30 +6,24 @@ const baseGoogleUrl = 'https://maps.googleapis.com/maps/api/place/findplacefromt
 const endGoogleUrl = '&inputtype=textquery&fields=geometry&key=AIzaSyB8KZZQcWwvgVaErPux7jjl1RnsVs8EuTg'
 
 
-
-// let checkUserAddress = () => {
-
-//     fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${addressParameter}&key=AIzaSyB8KZZQcWwvgVaErPux7jjl1RnsVs8EuTg`)
-// }
 const createSearchParameter = (input) => {
     return input.split(' ').join('+')
 }
-
 
 const combineAllUrls = (q) => {
     return baseGoogleUrl + createSearchParameter(q) + endGoogleUrl
 }
 
-
 app.get('/searchAddress', (req, res) => {
-    // console.log(createSearchParameter(req.query.q))
     console.log(combineAllUrls(req.query.q))
     request({url: combineAllUrls(req.query.q), json: true}, (err, response) => {
-        let latLong = response.body.candidates[0].geometry.location
-        res.send(response.body.status == "OK" ? latLong : "sorry, no results match that search")
+        if (response.body.status == "OK") {
+            res.send(response.body.candidates[0].geometry.location)
+        } else {
+            res.send({error: 'Sorry, that isnt a valid address.'})
+        }
     })
 })
-
 
 
 app.listen(3001, () => console.log('listening on 3001'))
